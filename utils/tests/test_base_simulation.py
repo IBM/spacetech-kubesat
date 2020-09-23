1a742b9d208e477c09be9b4d19b89aab780eb3bd
Minsik Lee <mslee@us.ibm.com>
2020-09-22 23:23:32 -0400

    fix test_base_simulation

--- utils/tests/test_base_simulation.py
+++ utils/tests/test_base_simulation.py
@@ -1,9 +1,8 @@
 """
-Tests for the Orbit simulation class. Expects that a nats server is running on 0.0.0.0:4222 and has user "a"
-with password "b". This can be done by running "nats-server --user a --pass b" in terminal.
+Tests for the BaseSimulation class.
 """
 
-import asyncio 
+import asyncio
 import unittest
 from unittest import TestCase
 from time import sleep
@@ -14,80 +13,81 @@ import os
 from kubesat.base_simulation import BaseSimulation
 from kubesat.validation import MessageSchemas, SharedStorageSchemas
 
+
 class Tests(TestCase):
     """
-    Testing the baseSimulation. TODO: Was not able yet to automate this test. need to check for output and need to 
-    send a message from a separate program.
+    Testing the BaseSimulation.
     """
 
-    def test_register_nats(self):
+    def test_register_subscribe_nats_callback(self):
         """
-        Testing whether registering routes for NATS works
+        Testing whether registering a NATS subscribe works
         """
 
-        sim = BaseSimulation("template_service",SharedStorageSchemas.TEMPLATE_STORAGE)
+        sim = BaseSimulation("template_service",
+                             SharedStorageSchemas.TEMPLATE_STORAGE)
 
         @sim.subscribe_nats_callback("publish.test", MessageSchemas.TEST_MESSAGE)
-        async def test_one(msg, nats, shared_storage, logger):
+        async def test_func(msg, nats, shared_storage, logger):
             pass
 
-        self.assertEqual(len(sim._nats_routes), 2)
-        self.assertEqual(len(sim._unsubscribe_nats_routes), 2)
+        self.assertEqual(len(sim._registered_callbacks), 3)
+        self.assertEqual(len(sim._unsubscribe_nats_routes), 3)
 
-    def test_register_nats_data(self):
+    def test_register_subscribe_data_callback(self):
         """
-        Testing whether registering a NATS data route works
+        Testing whether registering a data subscribe works
         """
 
-        sim = BaseSimulation("template_service",SharedStorageSchemas.TEMPLATE_STORAGE)
+        sim = BaseSimulation("template_service",
+                             SharedStorageSchemas.TEMPLATE_STORAGE)
 
         @sim.subscribe_data_callback("data.test", MessageSchemas.TEST_MESSAGE)
-        async def test_two(msg, nats, shared_storage, logger):
+        async def test_func(msg, nats, shared_storage, logger):
             pass
 
-        self.assertEqual(len(sim._nats_routes), 2)
-        self.assertEqual(len(sim._unsubscribe_nats_routes), 2)
+        self.assertEqual(len(sim._registered_callbacks), 3)
+        self.assertEqual(len(sim._unsubscribe_nats_routes), 3)
 
-    def test_register_request(self):
+    def test_register_request_nats_callback(self):
         """
         Testing whether registering requests works
         """
 
-        sim = BaseSimulation("template_service",SharedStorageSchemas.TEMPLATE_STORAGE)
+        sim = BaseSimulation("template_service",
+                             SharedStorageSchemas.TEMPLATE_STORAGE)
 
         @sim.request_nats_callback("data.test", MessageSchemas.TEST_MESSAGE)
-        async def test_two(msg, nats, shared_storage, logger):
+        async def test_func(msg, nats, shared_storage, logger):
             pass
 
-        self.assertEqual(len(sim._nats_routes), 2)
-        self.assertEqual(len(sim._unsubscribe_nats_routes), 2)
+        self.assertEqual(len(sim._registered_callbacks), 3)
+        self.assertEqual(len(sim._unsubscribe_nats_routes), 3)
 
-    def test_register_schedule(self):
+    def test_register_schedule_callback(self):
         """
-        Testing whether scheduling a callback
+        Testing whether registering scheduling works
         """
 
-        sim = BaseSimulation("template_service",SharedStorageSchemas.TEMPLATE_STORAGE)
+        sim = BaseSimulation("template_service",
+                             SharedStorageSchemas.TEMPLATE_STORAGE)
 
         @sim.schedule_callback(3)
-        async def test_two(nats, shared_storage, logger):
+        async def test_func(nats, shared_storage, logger):
             pass
 
-        self.assertEqual(len(sim._nats_routes), 2)
-        self.assertEqual(len(sim._unsubscribe_nats_routes), 1)
+        self.assertEqual(len(sim._registered_callbacks), 3)
 
     def test_register_startup_callback(self):
         """
         Testing whether the startup callback registration works
         """
 
-        sim = BaseSimulation("template_service",SharedStorageSchemas.TEMPLATE_STORAGE)
+        sim = BaseSimulation("template_service",
+                             SharedStorageSchemas.TEMPLATE_STORAGE)
 
         @sim.startup_callback
         async def startup(nats, shared_storage, logger):
             pass
 
         self.assertEqual(sim._startup_callback, startup)
-
-
-        
